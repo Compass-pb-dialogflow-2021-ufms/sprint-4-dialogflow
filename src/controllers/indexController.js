acab15a0fed44d84fe2e0a2afe9d90b5a0af9ccb
Tiago Seiti Fugishima <tiago.fugishima@gmail.com>
2021-12-17 10:05:58 -0400

    Commit 6 - Switcher para as intents e fluxo de conversa da Default Welcome Intent e Default Fallback Intent.

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,5 +1,103 @@
-const main = (req, res) => {
+const usuariosController = require('./usuariosController')
 
+const catchText = {
+    fulfillmentText: [
+        'Oh não, parece que um de nossos serviços está fora do ar !!!\n'
+        + 'Por favor tente novamente mais tarde.'
+    ]
+}
+
+const switchIntent = (req, res, intent, userId) => {
+    switch (intent) {
+        case 'Default Welcome Intent':
+            defaultWelcomeIntent(req, res, userId)
+            break
+        case 'Default Fallback Intent':
+            defaultFallbackIntent(req, res)
+            break
+        case 'Ajuda Intent':
+            ajudaIntent(req, res)
+            break
+        case 'Cotação Intent':
+            cotacaoIntent(req, res)
+            break
+        case 'Conversão Intent':
+            conversaoIntent(req, res)
+            break
+    }
+}
+
+const defaultWelcomeIntent = async (req, res, userId) => {
+    try {
+        let response;
+        const userNome = req.body.queryResult.parameters.usuario.name
+
+        if (usuariosController.getVerificado()) {
+            response = {
+                fulfillmentText: `Bem-vindo(a) ${usuariosController.getNome()}, como posso te ajudar hoje ?`
+            }
+        } else if (userNome && userNome !== '') {
+            response = {
+                fulfillmentText: [
+                    'Como é sua primeira vez utilizando o sistema vou me introduzir.\n\n'
+                    + 'Sou o seu assistente pessoal para cotações e converções monetárias, posso te informar quanto o dólar está valendo hoje por exemplo.\n'
+                    + 'Mas para verificar com mais detalhes todas minhas funções é so digitar ajuda ou pedir pelo menu.'
+                ]
+            }
+
+            await usuariosController.inserirUsuario(userNome, userId)
+        }else {
+            response = {
+                fulfillmentText: [
+                    'Olá, estou aqui para te ajudar nas converções monetárias.\n\n'
+                    + 'Mas antes me informe seu nome para que possamos conversar melhor.'
+                ]
+            }
+        }
+
+        res.send(response)
+    } catch {
+        res.send(catchText)
+    }
+}
+
+const defaultFallbackIntent = (req, res) => {
+    const response = {
+        fulfillmentText: [
+            `Desculpe ${usuariosController.getNome()} mas eu não entendi.\n`
+            + 'Se estiver com dificuldades é so digitar ajuda ou pedir pelo menu.'
+        ]
+    }
+
+    res.send(response)
+}
+
+const ajudaIntent = (req, res) => {
+
+}
+
+const cotacaoIntent = (req, res) => {
+
+}
+
+const conversaoIntent = (req, res) => {
+
+}
+
+const main = async (req, res) => {
+    try {
+        const userId = req.body.originalDetectIntentRequest.payload.data.source.userId
+        const intent = req.body.queryResult.intent.displayName
+
+        await usuariosController.verificarUsuario(userId)
+        if (usuariosController.getVerificado()) {
+            switchIntent(req, res, intent, userId)
+        } else {
+            defaultWelcomeIntent(req, res, userId)
+        }
+    } catch {
+        res.send(catchText)
+    }
 }
 
 module.exports = {
