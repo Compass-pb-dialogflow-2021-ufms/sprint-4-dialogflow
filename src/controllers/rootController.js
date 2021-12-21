f555b746751bc136cde65fda3070517efb6a4a51
Tiago Seiti Fugishima <tiago.fugishima@gmail.com>
2021-12-21 16:13:07 -0400

    Commit 1 - Criado o switch para as intents e os métodos para lidar com o fluxo de conversa para as 5 intents obrigatórias (Saudação, Ajuda, Sobre mim, Despedida e Fallback).

--- src/controllers/rootController.js
+++ src/controllers/rootController.js
@@ -1,5 +1,37 @@
-const main = (req, res) => {
+const switchIntent = (req, res, intent) => {
+    switch (intent) {
+        case 'Default Welcome Intent':
+            defaultWelcomeIntent(req, res)
+            break
+        case 'Default Fallback Intent':
+            defaultFallbackIntent(req, res)
+            break
+        case 'Help Intent':
+            helpIntent(req, res)
+            break
+        case 'About me Intent':
+            aboutMeIntent(req, res)
+            break
+        case 'Farewell Intent':
+            farewellIntent(req, res)
+            break
+    }
+}
 
+const defaultWelcomeIntent = (req, res) => {}
+
+const defaultFallbackIntent = (req, res) => {}
+
+const helpIntent = (req, res) => {}
+
+const aboutMeIntent = (req, res) => {}
+
+const farewellIntent = (req, res) => {}
+
+const main = (req, res) => {
+    const intent = req.body.queryResult.intent.displayName
+
+    switchIntent(req, res, intent)
 }
 
 module.exports = {
