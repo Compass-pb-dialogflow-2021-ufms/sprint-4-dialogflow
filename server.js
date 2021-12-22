65d5493f27ba5011644cee96331f4d966af7a761
Victor <victoor.landim@gmail.com>
2021-12-22 11:48:41 -0400

    Arrumando url de conexao do banco

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD);
 
 const controller = require('./controller/controller')
 
@@ -16,9 +20,7 @@ app.get('/', function (req, res) {
 
 app.post("/", controller)
 
-mongoose.connect(
-    'mongodb+srv://vdlandim:123456abc@nodejscluster.ygy5j.mongodb.net/compassoVoosDB?retryWrites=true&w=majority'
-).then(() => {
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@nodejscluster.ygy5j.mongodb.net/compassoVoosDB?retryWrites=true&w=majority`).then(() => {
     console.log("Conectado ao banco contatoDB")
     app.listen(process.env.PORT || 3000, () => {
         console.log('Rodando em  http://localhost:3000');
