4c2ab2ae873f5b82e221d6a5d15bdf4687e85cf4
Tiago Seiti Fugishima <tiago.fugishima@gmail.com>
2021-12-22 05:39:40 -0400

    Commit 5 - Fluxo de conversa para procurar uma passage,, resultado da busca emulado.

--- src/controllers/rootController.js
+++ src/controllers/rootController.js
@@ -1,3 +1,5 @@
+const { search, reservation, checkin, status } = require('../API/connection')
+
 const switchIntent = (req, res, intent) => {
     switch (intent) {
         case 'Default Welcome Intent':
@@ -14,6 +16,21 @@ const switchIntent = (req, res, intent) => {
             break
         case 'Farewell Intent':
             farewellIntent(req, res)
+            break
+        case 'Start Search Intent':
+            startSearchIntent(req, res)
+            break
+        case 'Where From Intent':
+            whereFromIntent(req, res)
+            break
+        case 'Where To Intent':
+            whereToIntent(req, res)
+            break
+        case 'Departure Date Intent':
+            departureDateIntent(req, res)
+            break
+        case 'Return Date Intent':
+            returnDateIntent(req, res)
             break
     }
 }
@@ -74,6 +91,107 @@ const farewellIntent = (req, res) => {
         fulfillmentText: [
               'Caso precise de mim estarei sempre aqui para te dar suporte com suas passagens aéreas.\n\n'
             + 'Espero ter te ajudado desta vez e até logo.'
+        ]
+    }
+
+    res.send(response)
+}
+
+const startSearchIntent = (req, res) => {
+    const response = {
+        fulfillmentText: [
+              'Para começar a busca de passagens para você, irei fazer algumas perguntas.\n\n'
+            + 'De qual aeroporto vai ser a partida ?'
+        ]
+    }
+
+    res.send(response)
+}
+
+const whereFromIntent = (req, res) => {
+    const response = {
+        fulfillmentText:[
+            'E em qual vai desembarcar ?'
+        ]
+    }
+
+    res.send(response)
+}
+
+const whereToIntent = (req, res) => {
+    const whereFrom = req.body.queryResult.outputContexts[1].parameters.whereFrom
+        , whereTo   = req.body.queryResult.outputContexts[0].parameters.whereTo
+
+    const response = {
+        fulfillmentText: [
+              `Anotado de ${whereFrom} para ${whereTo}.\n\n`
+            + 'Qual será a data de ida ?'
+        ]
+    }
+
+    res.send(response)
+}
+
+const departureDateIntent = (req, res) => {
+    const response = {
+        fulfillmentText: [
+            'E para finalizar qual a data para a volta ?'
+        ]
+    }
+
+    res.send(response)
+}
+
+const returnDateIntent = (req, res) => {
+    console.log(req.body.queryResult.outputContexts)
+    const whereFrom     = req.body.queryResult.outputContexts[0].parameters.whereFrom
+        , whereTo       = req.body.queryResult.outputContexts[0].parameters.whereTo
+        , departureDate = (req.body.queryResult.outputContexts[0].parameters.departureDate).split('T')[0]
+        , returnDate    = (req.body.queryResult.outputContexts[0].parameters.returnDate).split('T')[0]
+
+    const response = {
+        fulfillmentText: [
+              'Veja as passagens que econtrei para você:\n\n'
+            + '[OPÇÂO 1]\n'
+            + 'Companhia: Azul\n'
+            + 'Ida:\n'
+            + `    Local: ${whereFrom}\n`
+            + `    Data: ${departureDate}\n`
+            + '    Hora de embarque: 12:35\n'
+            + '    Poltronas: 9E 1F\n'
+            + 'Volta:\n'
+            + `    Local: ${whereTo}\n`
+            + `    Data: ${returnDate}\n`
+            + '    Hora de embarque: 18:35\n'
+            + '    Poltronas: 2F\n'
+            + 'Preço: R$ 1252,23\n\n'
+            + '[OPÇÂO 2]\n'
+            + 'Companhia: Gol\n'
+            + 'Ida:\n'
+            + `    Local: ${whereFrom}\n`
+            + `    Data: ${departureDate}\n`
+            + '    Hora de embarque: 08:25\n'
+            + '    Poltronas: 3B 8C 9C\n'
+            + 'Volta:\n'
+            + `    Local: ${whereTo}\n`
+            + `    Data: ${returnDate}\n`
+            + '    Hora de embarque: 12:35\n'
+            + '    Poltronas: 4A\n'
+            + 'Preço: R$ 1322,18\n\n'
+            + '[OPÇÂO 3]'
+            + 'Companhia: LATAM\n'
+            + 'Ida:\n'
+            + `    Local: ${whereFrom}\n`
+            + `    Data: ${departureDate}\n`
+            + '    Hora de embarque: 18:30\n'
+            + '    Poltronas: 1A 8A 3F\n'
+            + 'Volta:\n'
+            + `    Local: ${whereTo}\n`
+            + `    Data: ${returnDate}\n`
+            + '    Hora de embarque: 22:15\n'
+            + '    Poltronas: 3D\n'
+            + 'Preço: R$ 1126,49\n\n'
+            + 'Qual dessas opções deseja reservar ?'
         ]
     }
 
